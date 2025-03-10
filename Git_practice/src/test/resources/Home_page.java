e13f5dd4741e51d51a639ff76421d96ca3428a23
sumit <sumit@Sneh>
2025-03-10 18:32:46 +0530

    these are new changes

--- Git_practice/src/test/resources/Home_page.java
+++ Git_practice/src/test/resources/Home_page.java
@@ -3,6 +3,7 @@ public class Home_page {
 
 	public static void main(String[] args) {
 		System.out.println("this is home page");
+		System.out.println("work on this");
 
 	}
 
--- Git_practice/src/test/resources/Login_page.java
+++ Git_practice/src/test/resources/Login_page.java
@@ -5,6 +5,7 @@ public class Login_page {
 		System.out.println("this is login page");
 		System.out.println("this page user can enter user name");
 		System.out.println("user can enter password");
+		System.out.println("i am yor team lead");
 
 	}
 
