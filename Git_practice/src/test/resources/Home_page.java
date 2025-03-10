1c22fcbe91569ef3597798de94d7b129e1535640
sumit <sumit@Sneh>
2025-03-10 17:22:31 +0530

    this is 1st build

---
+++ Git_practice/src/test/resources/Home_page.java
@@ -0,0 +1,9 @@
+
+public class Home_page {
+
+	public static void main(String[] args) {
+		System.out.println("this is home page");
+
+	}
+
+}
---
+++ Git_practice/src/test/resources/Login_page.java
@@ -0,0 +1,9 @@
+
+public class Login_page {
+
+	public static void main(String[] args) {
+		System.out.println("this is log in page");
+
+	}
+
+}
