31331c81d1e0abffa334e9b56fc2256a031b8bb4
sumit <sumit@Sneh>
2025-03-10 17:40:03 +0530

    this is build two

--- Git_practice/src/test/resources/Login_page.java
+++ Git_practice/src/test/resources/Login_page.java
@@ -2,7 +2,9 @@
 public class Login_page {
 
 	public static void main(String[] args) {
-		System.out.println("this is log in page");
+		System.out.println("this is login page");
+		System.out.println("this page user can enter user name");
+		System.out.println("user can enter password");
 
 	}
 
